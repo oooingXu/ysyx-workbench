2b8b46e986dfb471fafe3d0d5d8d4b63d93e8a38
oooing <202301202097@stumail.sztu.edu.cn>
2025-10-04 22:28:31 +0800

    Modified the SEEPC to add sram_read

--- npc/src/main/sim/SEEPC.scala
+++ npc/src/main/sim/SEEPC.scala
@@ -10,26 +10,35 @@ class SEEPC extends BlackBox with HasBlackBoxInline{
     val pc    = Input(UInt(32.W))
     val dnpc  = Input(UInt(32.W))
     val inst  = Input(UInt(32.W))
+    val wbu_sram_data = new WBU_SRAM_DATA()
   })
 
   setInline(
     "seepc.sv",
   """`ifdef VERILATOR
-    |import "DPI-C" function void pipeline_state(input int pc, input int dnpc, input int inst, input int valid);
+    |import "DPI-C" function void pipeline_state(input int pc, input int dnpc, input int inst, input int valid, input int araddr, input int arvalid, input int arsize, input int awaddr, input int wdata, input int awvalid, input int wstrb);
     |`endif
     | module SEEPC(
     |   input clock,
     |   input valid,
     |   input [31:0] pc,
+    |   input [31:0] dnpc,
     |   input [31:0] inst,
-    |   input [31:0] dnpc
+    |   input [31:0] wbu_sram_data_araddr,
+    |   input [31:0] wbu_sram_data_awaddr,
+    |   input [31:0] wbu_sram_data_wdata,
+    |   input [3:0]  wbu_sram_data_wstrb,
+    |   input [2:0]  wbu_sram_data_arsize,
+    |   input        wbu_sram_data_arvalid,
+    |   input        wbu_sram_data_awvalid
     | );
     | `ifdef VERILATOR
     | always@(posedge clock) begin
-    |   pipeline_state(pc, dnpc, inst, {31'b0, valid});
+    |   pipeline_state(pc, dnpc, inst, {31'b0, valid}, wbu_sram_data_araddr, {31'b0, wbu_sram_data_arvalid}, {29'b0, wbu_sram_data_arsize}, wbu_sram_data_awaddr, wbu_sram_data_wdata, {31'b0, wbu_sram_data_awvalid}, {28'b0, wbu_sram_data_wstrb});
     | end
     |`endif
     |
     | endmodule
   """.stripMargin)
 }
+
