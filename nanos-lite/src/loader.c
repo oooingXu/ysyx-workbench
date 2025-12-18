c589aaa17c61c18e2b29caf23bad9dac3dddfd3f
oooing <202301202097@stumail.sztu.edu.cn>
2025-12-18 22:57:49 +0800

    NJU-ProjectN/nanos-lite ics2021 initialized
    NJU-ProjectN/nanos-lite 2a141760e31be246a7316942293a97873925bc2f Makefile: use header files in newlib-navy

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,28 +9,9 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-/*
 static uintptr_t loader(PCB *pcb, const char *filename) {
   TODO();
   return 0;
-}
-*/
-
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  uint32_t phdr_size = sizeof(Elf_Phdr);
-  Elf_Ehdr *ehdr = malloc(sizeof(Elf_Ehdr));
-  Elf_Phdr *phdr = malloc(phdr_size);
-  ramdisk_read((void*)ehdr, 0, sizeof(Elf_Ehdr));
-  assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
-//  assert(EXPECT_TYPE == ehdr->e_machine);
-  uint32_t phdr_num = ehdr->e_phnum;
-  for(int i = 0; i < phdr_num; i++) {
-    ramdisk_read((void*)phdr, ehdr->e_phoff + i * phdr_size, phdr_size);
-    if(phdr->p_type != PT_LOAD) continue;
-    ramdisk_read((void*)phdr->p_vaddr, phdr->p_offset, phdr->p_filesz);
-    memset((void*)phdr->p_vaddr + phdr->p_filesz, 0, phdr->p_memsz - phdr->p_filesz);
-  }
-  return ehdr->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
