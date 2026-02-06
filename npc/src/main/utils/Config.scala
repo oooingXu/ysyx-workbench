d9330ba69546935e742d90d67902fa240ee131fb
oooing <202301202097@stumail.sztu.edu.cn>
2026-02-06 16:51:24 +0800

    Modified the chisel Config.scala to auto Add CONFIG in Makefile

--- npc/src/main/utils/Config.scala
+++ npc/src/main/utils/Config.scala
@@ -1,11 +1,11 @@
 package npc
 
 object Config {
-  val useNPCSim  = true
-  val useICache  = true
-  val useDebug   = true
+  val useNPCSim  = sys.env.get("CONFIG_NPC").getOrElse("n") == "y"
+  val useICache  = sys.env.get("CONFIG_ICACHE").getOrElse("n") == "y"
+  val useDebug   = sys.env.get("CONFIG_VERILATOR_SIM").getOrElse("n") == "y"
   val useKonata  = sys.env.get("CONFIG_KONATA").getOrElse("n") == "y"
 
   // isa RV32E
-  val RV32E      = false
+  val RV32E      = sys.env.get("CONFIG_RVE").getOrElse("n") == "y"
 }
